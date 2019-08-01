8ba330c5b54f627e23c8232ff75a872cb8fb2d1d
Simon <sam_moshenko@mail.ru>
2019-08-01 17:08:23 +0600

    Add MotionTracker

--- MovementTracker/MotionTracker/MotionTracker.swift
+++ MovementTracker/MotionTracker/MotionTracker.swift
@@ -8,7 +8,62 @@
 
 import Foundation
 import CoreMotion
+import RxSwift
 
-class MotionTracker {
+public class MotionTracker {
+    public let motionSecondsObservable = PublishSubject<Int>()
+    public let noMotionSecondsObservable = PublishSubject<Int>()
+    private let minimumChange = 1.0
+    private let manager = CMMotionManager()
+    private var timer: Timer?
     
+    public func trackMotion() {
+        if manager.isDeviceMotionAvailable {
+            manager.deviceMotionUpdateInterval = 1.0 / 60.0
+            manager.showsDeviceMovementDisplay = true
+            manager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+            
+            var previousSum: Double? = nil
+            var motionSeconds = 0
+            var noMotionSeconds = 0
+            // Configure a timer to fetch the motion data.
+            timer = Timer(fire: Date(), interval: 1, repeats: true,
+                               block: { [weak self] (timer) in
+                                guard let self = self else { return }
+                                
+                                if let data = self.manager.deviceMotion {
+                                    // Get the attitude relative to the magnetic north reference frame.
+                                    let x = data.attitude.pitch
+                                    let y = data.attitude.roll
+                                    let z = data.attitude.yaw
+                                    let sum = x + y + z
+                                    if let previousSumUnwrapped = previousSum {
+                                        let change = previousSumUnwrapped - sum
+                                        if change > self.minimumChange {
+                                            motionSeconds += 1
+                                            previousSum = sum
+                                        } else {
+                                            previousSum = nil
+                                            motionSeconds = 0
+                                        }
+                                        noMotionSeconds = 0
+                                    } else {
+                                        noMotionSeconds += 1
+                                        motionSeconds = 0
+                                    }
+                                    self.continueMotion(motionSeconds, noMotionSeconds: noMotionSeconds)
+                                }
+            })
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(timer!, forMode: RunLoop.Mode.default)
+        } else {
+            debugPrint("Device motion is unavailable")
+        }
+    }
+    
+    private func continueMotion(_ motionSeconds: Int, noMotionSeconds: Int) {
+        motionSecondsObservable.onNext(motionSeconds)
+        noMotionSecondsObservable.onNext(noMotionSeconds)
+    }
 }
--- MovementTracker/MotionTracker/MotionTrackerViewModel.swift
+++ MovementTracker/MotionTracker/MotionTrackerViewModel.swift
@@ -10,6 +10,58 @@ import Foundation
 import RxSwift
 
 internal class MotionTrackerViewModel {
-    let statusText = PublishSubject<String>()
+    private let successfullMovementTime = 10
+    private let disposeBag = DisposeBag()
+    private let motionTracker: MotionTracker
     
+    let statusText = PublishSubject<String>()
+    private let movementSuccessfull = PublishSubject<Void>()
+    private let movementStarted = PublishSubject<Void>()
+    private let noMovement = PublishSubject<Void>()
+    
+    init(motionTracker: MotionTracker) {
+        self.motionTracker = motionTracker
+        setupMotionTracker()
+        setupStatusText()
+    }
+    
+    private func setupStatusText() {
+        movementSuccessfull.map {
+            "Энергия успешно потрачена."
+            }.bind(to: statusText)
+            .disposed(by: disposeBag)
+        movementStarted.map {
+            "Вы тратите энергию"
+            }.bind(to: statusText)
+            .disposed(by: disposeBag)
+        noMovement.map {
+            "Пожалуйста потратьте энергию"
+            }.bind(to: statusText)
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupMotionTracker() {
+        motionTracker.trackMotion()
+        Observable.combineLatest(motionTracker.motionSecondsObservable, motionTracker.noMotionSecondsObservable)
+            .do(onNext: { [weak self] motionSeconds, noMotionSeconds in
+                guard let self = self else { return }
+                let isFactorOfSuccessfullMovementTime = motionSeconds % self.successfullMovementTime == 0
+                if (isFactorOfSuccessfullMovementTime && motionSeconds != 0) {
+                    self.movementSuccessfull.onNext(())
+                }
+            })
+            .do(onNext: { [weak self] motionSeconds, noMotionSeconds in
+                guard let self = self else { return }
+                if (motionSeconds > 0) {
+                    self.movementStarted.onNext(())
+                }
+            })
+            .do(onNext: { [weak self] motionSeconds, noMotionSeconds in
+                guard let self = self else { return }
+                if (motionSeconds == 0 && noMotionSeconds >= 5) {
+                    self.noMovement.onNext(())
+                }
+            })
+            .subscribe().disposed(by: disposeBag)
+    }
 }
